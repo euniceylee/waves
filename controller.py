c9692d1242e2dfcd5636bf0da6939e5ae9b2416e
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2017-05-28 12:22:38 -0500

    Adds global while lood for buttons

--- controller.py
+++ controller.py
@@ -11,32 +11,30 @@ class ButtonRecorder:
 		self.pin = pin
 		print(self.pin)
 		GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+		self.input_state = GPIO.input(self.pin)
+	
+	def getInputState(self):
+		input_state = GPIO.input(self.pin)
+		return input_state
 
 	def start(self): 
-		while True:
-			input_state = GPIO.input(self.pin)
-			if input_state == False:
-				print("Say something! (:")
-				subprocess.Popen(["python","/home/pi/Desktop/waves/killer.py"])
-				print("Recording now")
-				call(["arecord","/home/pi/Desktop/waves/audio3.mp3", "-D", "sysdefault:CARD=1"])
-				print("Recording stopped")
-				createViz(self.pin)  
-				print("Viz Created")
+		print("Say something! (:")
+		subprocess.Popen(["python","/home/pi/Desktop/waves/killer.py"])
+		print("Recording now")
+		call(["arecord","/home/pi/Desktop/waves/audio3.mp3", "-D", "sysdefault:CARD=1"])
+		print("Recording stopped")
+		createViz(self.pin)  
+		print("Viz Created")
 
-rec = ButtonRecorder(23)
-rec.start() 
 
-# while True:
-#     input_state = GPIO.input(23)
-#     if input_state == False:
-#     	print("Say something! (:")
-#     	subprocess.Popen(["python","/home/pi/Desktop/waves/killer.py"])
-# # 	print("Recording now")
-# #         call(["arecord","/home/pi/Desktop/waves/audio3.mp3", "-D", "sysdefault:CARD=1"])
-# # 	print("Recording stopped")
-# 	# subprocess.Popen(["python","/home/pi/Desktop/waves/viz.py"]) 
-# # 	createViz()       
-# #         #launch printer.py
-# # 	print("Viz stopped")
-# #         time.sleep(0.2)
+rec23 = ButtonRecorder(23)
+s23 = rec23.getInputState()
+
+rec24 = ButtonRecorder(24)
+s24 = rec24.getInputState()
+
+while True:
+	if (s23 == False):
+		rec23.start()
+	elif (s24 == False):
+		rec24.start()
