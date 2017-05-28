c989b8d1848b80599b96151fb600c3bc4216889a
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2017-05-28 11:44:35 -0500

    Utilizes event bindings to detect falling/rising buttons

--- controller.py
+++ controller.py
@@ -1,21 +1,49 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as gpio
 import time
 from subprocess import call
 import subprocess
+from viz import *
 
-GPIO.setmode(GPIO.BCM)
+gpio.setmode(GPIO.BCM)
 
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+class ButtonRecorder(pin): 
+	def __init__(self, pin): 
+		self.pin = pin
+		print(self.pin)
+        gpio.setup(self.pin, gpio.IN, pull_up_down=gpio.PUD_UP) 
 
-while True:
-    input_state = GPIO.input(23)
-    if input_state == False:
-    	print("Say something! (:")
-    	subprocess.Popen(["python","/home/pi/Desktop/waves/killer.py"])
-	print("Recording now")
+    def start(self): 
+        gpio.add_event_detect(self.pin, gpio.FALLING, callback=self.falling, bouncetime=10) 
+
+    def rising(self, channel): 
+        gpio.remove_event_detect(self.pin) 
+        print 'Button up' 
+        gpio.add_event_detect(self.pin, gpio.FALLING, callback=self.falling, bouncetime=10) 
+		print("Recording stopped")
+		call(["killall", "-KILL", "arecord"])
+		createViz(self.pin)   
+
+    def falling(self, channel): 
+		print(self.pin)
+        gpio.remove_event_detect(self.pin) 
+        print 'Button down' 
+        gpio.add_event_detect(self.pin, gpio.RISING, callback=self.rising, bouncetime=10) 
+		print("Recording now")
         call(["arecord","/home/pi/Desktop/waves/audio3.mp3", "-D", "sysdefault:CARD=1"])
-	print("Recording stopped")
-	subprocess.Popen(["python","/home/pi/Desktop/waves/viz.py"])        
-        #launch printer.py
-	print("Viz stopped")
-        time.sleep(0.2)
+
+rec = ButtonRecorder(23)
+rec.start() 
+
+# while True:
+#     input_state = GPIO.input(23)
+#     if input_state == False:
+#     	print("Say something! (:")
+#     	subprocess.Popen(["python","/home/pi/Desktop/waves/killer.py"])
+# 	print("Recording now")
+#         call(["arecord","/home/pi/Desktop/waves/audio3.mp3", "-D", "sysdefault:CARD=1"])
+# 	print("Recording stopped")
+# 	# subprocess.Popen(["python","/home/pi/Desktop/waves/viz.py"]) 
+# 	createViz()       
+#         #launch printer.py
+# 	print("Viz stopped")
+#         time.sleep(0.2)
--- viz.py
+++ viz.py
@@ -8,59 +8,59 @@ import os
 
 print("Viz script has started")
 
+def createViz(pin):
+    src = "audio3.mp3"
 
-src = "audio3.mp3"
+    audio = AudioSegment.from_file(src)
+    data = np.fromstring(audio._data, np.int16)
+    fs = audio.frame_rate
 
-audio = AudioSegment.from_file(src)
-data = np.fromstring(audio._data, np.int16)
-fs = audio.frame_rate
+    BARS = 500
+    BAR_HEIGHT = 300
+    LINE_WIDTH = 5
 
-BARS = 500
-BAR_HEIGHT = 300
-LINE_WIDTH = 5
+    length = len(data)
+    RATIO = length/BARS
 
-length = len(data)
-RATIO = length/BARS
+    count = 0
+    maximum_item = 0
+    max_array = []
+    highest_line = 0
 
-count = 0
-maximum_item = 0
-max_array = []
-highest_line = 0
+    for d in data:
+        if count < RATIO:
+            count = count + 1
 
-for d in data:
-    if count < RATIO:
-        count = count + 1
+            if abs(d) > maximum_item:
+                maximum_item = abs(d)
+        else:
+            max_array.append(maximum_item)
 
-        if abs(d) > maximum_item:
-            maximum_item = abs(d)
-    else:
-        max_array.append(maximum_item)
+            if maximum_item > highest_line:
+                highest_line = maximum_item
 
-        if maximum_item > highest_line:
-            highest_line = maximum_item
+            maximum_item = 0
+            count = 1
 
-        maximum_item = 0
-        count = 1
+    line_ratio = highest_line/BAR_HEIGHT
 
-line_ratio = highest_line/BAR_HEIGHT
+    fnt = ImageFont.truetype('vcr.ttf', 50)
 
-fnt = ImageFont.truetype('vcr.ttf', 50)
+    im = Image.new('RGB', (BARS * LINE_WIDTH, BAR_HEIGHT), (255, 255, 255))
+    draw = ImageDraw.Draw(im)
+    draw.text((0,0), "WHAT'S YOUR MOTTO?", font=fnt, fill=(0,0,0,255))
 
-im = Image.new('RGB', (BARS * LINE_WIDTH, BAR_HEIGHT), (255, 255, 255))
-draw = ImageDraw.Draw(im)
-draw.text((0,0), "WHAT'S YOUR MOTTO?", font=fnt, fill=(0,0,0,255))
+    current_x = 1
+    for item in max_array:
+        item_height = item/line_ratio
 
-current_x = 1
-for item in max_array:
-    item_height = item/line_ratio
+        current_y = (BAR_HEIGHT - item_height)/2
+        draw.line((current_x, current_y, current_x, current_y + item_height), fill=(0, 0, 0), width=4)
 
-    current_y = (BAR_HEIGHT - item_height)/2
-    draw.line((current_x, current_y, current_x, current_y + item_height), fill=(0, 0, 0), width=4)
+        current_x = current_x + LINE_WIDTH
 
-    current_x = current_x + LINE_WIDTH
+    im.show()
 
-im.show()
+    im.save("audioViz3.bmp")
 
-im.save("audioViz3.bmp")
-
-call(["lpr","-o","fit-to-page","audioViz3.bmp"])
+    call(["lpr","-o","fit-to-page","audioViz3.bmp"])
