3aeac6e2bee11dd3b28c02dd7e4ad59588c229d2
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2017-05-28 12:02:38 -0500

    Adds class for instantiating the programs for a given pin

--- controller.py
+++ controller.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as gpio
+import RPi.GPIO as GPIO
 import time
 from subprocess import call
 import subprocess
@@ -10,26 +10,19 @@ class ButtonRecorder(pin):
 	def __init__(self, pin):
 		self.pin = pin
 		print(self.pin)
-		gpio.setup(self.pin, gpio.IN, pull_up_down=gpio.PUD_UP) 
+		GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 	def start(self): 
-		gpio.add_event_detect(self.pin, gpio.FALLING, callback=self.falling, bouncetime=10) 
-
-	def rising(self, channel): 
-		gpio.remove_event_detect(self.pin) 
-		print 'Button up' 
-		gpio.add_event_detect(self.pin, gpio.FALLING, callback=self.falling, bouncetime=10) 
-		print("Recording stopped")
-		call(["killall", "-KILL", "arecord"])
-		createViz(self.pin)   
-
-	def falling(self, channel): 
-		print(self.pin)
-		gpio.remove_event_detect(self.pin) 
-		print 'Button down' 
-		gpio.add_event_detect(self.pin, gpio.RISING, callback=self.rising, bouncetime=10) 
-		print("Recording now")
-		call(["arecord","/home/pi/Desktop/waves/audio3.mp3", "-D", "sysdefault:CARD=1"])
+		while True:
+			input_state = GPIO.input(self.pint)
+			if input_state == False:
+				print("Say something! (:")
+				subprocess.Popen(["python","/home/pi/Desktop/waves/killer.py"])
+				print("Recording now")
+				call(["arecord","/home/pi/Desktop/waves/audio3.mp3", "-D", "sysdefault:CARD=1"])
+				print("Recording stopped")
+				createViz(self.pin)  
+				print("Viz Created")
 
 rec = ButtonRecorder(23)
 rec.start() 
@@ -39,11 +32,11 @@ rec.start()
 #     if input_state == False:
 #     	print("Say something! (:")
 #     	subprocess.Popen(["python","/home/pi/Desktop/waves/killer.py"])
-# 	print("Recording now")
-#         call(["arecord","/home/pi/Desktop/waves/audio3.mp3", "-D", "sysdefault:CARD=1"])
-# 	print("Recording stopped")
+# # 	print("Recording now")
+# #         call(["arecord","/home/pi/Desktop/waves/audio3.mp3", "-D", "sysdefault:CARD=1"])
+# # 	print("Recording stopped")
 # 	# subprocess.Popen(["python","/home/pi/Desktop/waves/viz.py"]) 
-# 	createViz()       
-#         #launch printer.py
-# 	print("Viz stopped")
-#         time.sleep(0.2)
+# # 	createViz()       
+# #         #launch printer.py
+# # 	print("Viz stopped")
+# #         time.sleep(0.2)
